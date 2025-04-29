2ff7470465b6b889c3f0d044de2234791bd6d441
safiullah Yusufzai <safiullahyusufzai7@gmail.com>
2025-04-29 14:02:27 +0430

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I am Rich', textAlign: TextAlign.center),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Image(image: AssetImage('images/Diamond.jpg')),
+      ),
+    );
+  }
+}
